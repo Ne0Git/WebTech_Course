c8d60081932bd212aefb234f8a348c1ad879ae03
ne0 <iv.byvakin@gmail.com>
2022-12-11 14:38:22 +0000

    added models for question and answer along with some chore stuff for them

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo /etc/init.d/mysql start
+
+sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS ask_dev DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON ask_dev.* TO 'box'@'localhost';
+FLUSH PRIVILEGES;"
